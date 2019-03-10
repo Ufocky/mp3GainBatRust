be20544d283d95b862d697f0a8ca00b60bc22988
Ufocky <willy@geronde.pl>
2019-03-10 14:27:05 +0100

    Rumcajs needed for some help!

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         return;
     }
 
-    let files: Vec<String> = get_files(std::path::Path::new(&arg[2]));
+    let files: Vec<String> = get_files(std::path::Path::new(&arg[2]), &0);
 
     let mut output_file: String = "".to_string();
     add_line(&mut output_file, &"@echo off".to_string());
@@ -28,8 +28,7 @@ fn main() {
         add_line(&mut output_file, &format!("echo files left:{}", cnt));
     }
 
-    let result = std::fs::write(std::path::Path::new(&arg[1]), output_file);
-    if result.is_err() {
+    if std::fs::write(std::path::Path::new(&arg[1]), output_file).is_err() {
         println!(
             "Could not write to \"{}\". Is this correct file name?",
             arg[1]
@@ -43,15 +42,20 @@ fn add_line(so: &mut String, si: &String) {
     so.push_str(format!("{}\r\n", &si).as_str());
 }
 
-fn get_files(dir: &std::path::Path) -> Vec<String> {
+fn get_files(dir: &std::path::Path, sub_level: &usize) -> Vec<String> {
     let mut out: Vec<String> = Vec::new();
     if dir.is_dir() {
-        println!("scanning {} for mp3 files...", dir.display());
+        //let format_str = ; //format!("{: >1$}", "|_", sub_level);
+        println!(
+            "{}{}",
+            gen_dist_str(sub_level),
+            osstr_2_string(&dir.file_name())
+        );
         let dir_entries = std::fs::read_dir(dir).unwrap();
         for dir_entry in dir_entries {
             let dir_entry = dir_entry.unwrap().path();
             if dir_entry.is_dir() {
-                out.append(&mut get_files(dir_entry.as_path()));
+                out.append(&mut get_files(dir_entry.as_path(), &(sub_level + 1)));
             } else {
                 if check_extension(&dir_entry) {
                     out.push(dir_entry.to_str().unwrap_or("").to_string());
@@ -72,4 +76,26 @@ fn check_extension(path_buf: &std::path::PathBuf) -> bool {
         .unwrap_or("")
         .to_lowercase()
         == "mp3"
+}
+
+fn osstr_2_string(input: &std::option::Option<&std::ffi::OsStr>) -> String {
+    input
+        .unwrap_or(std::ffi::OsStr::new(""))
+        .to_str()
+        .unwrap_or("")
+        .to_string()
+}
+
+fn gen_dist_str(input: &usize) -> String {
+    let mut out: String = "".to_string();
+    if *input < 1 {
+        return "".to_string();
+    }
+    if *input > 2 {
+        for _ in 0..((*input) - 2) {
+            out += "| ";
+        }
+    }
+    out += "|-";
+    out
 }
